d95dbb824a2c46d0e42fac7447d86419d415d1e5
abdelhakim <abdelhakim.oumouchi@edu.ensa.dz>
2026-03-20 23:58:10 +0100

    fix probleme prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,6 +4,18 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasources: {
+      db: {
+        url:
+          process.env.DATABASE_URL ||
+          "postgresql://neondb_owner:npg_0ERAKrhdF2sI@ep-empty-river-aggj4bft.c-2.eu-central-1.aws.neon.tech:5432/neondb?sslmode=require"
+      }
+    }
+  })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
